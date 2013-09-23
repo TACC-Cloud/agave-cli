7c5d6d48559a0b1beb7ea4550d193526aa0eb583
Rion Dooley <dooley@tacc.utexas.edu>
2013-09-22 23:42:20 -0400

    Several syntax fixes and clarification in the files-* commands help statements.

--- bin/auth-common.sh
+++ bin/auth-common.sh
@@ -10,12 +10,12 @@
 filter_service_url() {
 	if [[ -z $hosturl ]]; then
 		if ((development)); then 
-			hosturl="$devurl/auth-v1"
+			hosturl="http://localhost/auth-v2"
 		else
 			hosturl="$baseurl/auth"
 		fi
 	fi
 	
-	hosturl=${hosturl%/}
+	hosturl=${hosturl}
 }
 
