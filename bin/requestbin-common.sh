3ed033bf66480d20b4758d5a39a2a690d9b90a60
Rion Dooley <dooley@tacc.utexas.edu>
2016-09-17 12:05:29 -0500

    Updating requestbin and tenants api to use SSL.

--- bin/requestbin-common.sh
+++ bin/requestbin-common.sh
@@ -10,9 +10,9 @@
 filter_service_url() {
 	if [[ -z $hosturl ]]; then
 		if ((development)); then 
-			hosturl="http://requestbin.agaveapi.co/"
+			hosturl="https://requestbin.agaveapi.co/"
 		else
-			hosturl="http://requestbin.agaveapi.co/"
+			hosturl="https://requestbin.agaveapi.co/"
 		fi
 	fi
 }
