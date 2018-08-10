65062733c93f0f7f9031fb0da18fb9770fe38170
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-08-10 14:40:50 -0500

    Redirect server's stdout to log file and improve clients-list test (#6)
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

--- tests/hack/serve_agave_mock_server.sh
+++ tests/hack/serve_agave_mock_server.sh
@@ -5,4 +5,5 @@ gunicorn --bind 0.0.0.0:5000 \
     --keyfile /agave-cli/tests/hack/dummy-key.pem \
     --pythonpath /agave-cli/tests/agave_mock_server wsgi \
     --log-file /agave-cli/tests/agave_mock_server.log \
+    --capture-output \
     --daemon
