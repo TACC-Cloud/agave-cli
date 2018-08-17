a97cd737c7b27118bb54ce6f25a372248713f3e9
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-08-17 14:40:36 -0500

    Add files-mkdir test
    Implemented mock server endpoint to test files-mkdir command and wrote test.
    
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

--- tests/agave_mock_server/agave_mock_server/filesmedia_endpoints.py
+++ tests/agave_mock_server/agave_mock_server/filesmedia_endpoints.py
@@ -11,9 +11,16 @@ from .validation_helpers import (basic_access_token_checks,
     basic_file_path_checks, basic_system_id_checks)
 
 
-# Sample response for "files-upload -S -S system -F file.ext /path" cli
+# Sample response for "files-mkdir -V -S system -N name /path".
+# curl -sk -H "Authorization: Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
+#   -X PUT -d "action=mkdir&path=name" \
+#   'https://api.tenant.org/files/v2/media/system/system//path?pretty=true'
+files_mkdir_response = response_template_to_json("files-mkdir.json")
+
+
+# Sample response for "files-upload -V -S system -F file.ext /path" cli
 # command.
-# curl -# -k -H "Authorization: Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxx" \
+# curl -# -k -H "Authorization: Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
 #   -X POST -F "fileToUpload=@file.ext" \
 #   'https://api.tenant.org/files/v2/media/system/system/path?pretty=true'
 files_upload_response = response_template_to_json("files-upload.json")
@@ -87,3 +94,46 @@ class AgaveFilesMedia(Resource):
         response = response.replace("{REMOTEPATH}", file_path)
         response = response.replace("{SYSTEMID}", system_id)
         return jsonify(json.loads(response)) 
+
+    def put(self, system_id, file_path):
+        """ Test files-mkdir command
+
+        curl -k -H "Authorization: Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
+            -X PUT -d "action=mkdir&path=name" \
+            'https://localhost:5000/files/v2/media/system/system//path?pretty=true'
+        """
+        # Test api access token.
+        token = request.headers.get("Authorization")
+        resp = basic_access_token_checks(token)
+        if resp:
+            return resp
+
+        # Test system ID.
+        resp = basic_system_id_checks(system_id)
+        if resp:
+            return resp
+
+        # Test file/dir path.
+        resp = basic_file_path_checks(file_path)
+        if resp:
+            return resp
+
+        # Test form data fields "action" and "path".
+        action = request.form.get("action")
+        if action != "mkdir":
+            resp = jsonify({"error": "Bad authorization header"})
+            resp.status_code = 400
+            return resp
+
+        new_dirname = request.form.get("path")
+        if new_dirname is None or new_dirname == "":
+            resp = jsonify({"error": "Bad authorization header"})
+            resp.status_code = 400
+            return resp
+
+        response = json.dumps(files_mkdir_response)                            
+        response = response.replace("{URL_ROOT}", request.url_root)
+        response = response.replace("{REMOTEPATH}", file_path)
+        response = response.replace("{SYSTEMID}", system_id)
+        response = response.replace("{DIRNAME}", new_dirname)
+        return jsonify(json.loads(response))
