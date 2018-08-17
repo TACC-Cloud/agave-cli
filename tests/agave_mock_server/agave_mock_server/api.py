e40a9c0ecd88847dd897bc75316fe49654b9d392
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-08-17 11:57:03 -0500

    Add files-list test
    Rename Dockerfile for development container.
    Improve "tests-setup" Makefile target.
    Implemented mock server endpoint to test files-list cli command.
    Cleaned up mock server source.
    
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

--- tests/agave_mock_server/agave_mock_server/api.py
+++ tests/agave_mock_server/agave_mock_server/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from .apps_endpoints import AgaveApps
 from .clients_endpoints import AgaveClients
 from .filesmedia_endpoints import AgaveFilesMedia
+from .fileslistings_endpoints import AgaveFilesListings
 from .postits_endpoints import AgavePostits
 
 app = Flask(__name__)
@@ -15,6 +16,9 @@ api.add_resource(AgaveClients, "/clients/v2/",
 api.add_resource(AgaveFilesMedia, 
     "/files/v2/media/system/<string:system_id>/<string:file_path>",
     "/files/v2/media/system/<string:system_id>//<string:file_path>")
+api.add_resource(AgaveFilesListings,
+    "/files/v2/listings/system/<string:system_id>/<string:file_path>",
+    "/files/v2/listings/system/<string:system_id>//<string:file_path>")
 api.add_resource(AgavePostits, "/postits/v2/")
 
 if __name__ == "__main__":
