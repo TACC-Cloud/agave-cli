3f53c7669cbd9bb3b2463fde9e6293510497788f
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-08-17 12:00:19 -0500

    Add files-list test (#20)
    Rename Dockerfile for development container.
    Improve "tests-setup" Makefile target.
    Implemented mock server endpoint to test files-list cli command.
    Cleaned up mock server source.
    
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

--- tests/agave_mock_server/agave_mock_server/filesmedia_endpoints.py
+++ tests/agave_mock_server/agave_mock_server/filesmedia_endpoints.py
@@ -7,6 +7,8 @@ import json
 from flask import jsonify, request
 from flask_restful import Resource
 from .response_templates import response_template_to_json
+from .validation_helpers import (basic_access_token_checks,                     
+    basic_file_path_checks, basic_system_id_checks)
 
 
 # Sample response for "files-upload -S -S system -F file.ext /path" cli
@@ -15,60 +17,6 @@ from .response_templates import response_template_to_json
 #   -X POST -F "fileToUpload=@file.ext" \
 #   'https://api.tenant.org/files/v2/media/system/system/path?pretty=true'
 files_upload_response = response_template_to_json("files-upload.json")
-
-
-def basic_access_token_checks(access_token=None):
-    """ Test validity of access token
-
-    Check whether the Authorization header from the request meets basic format
-    requirements.
-
-    INPUT
-    -----
-    access_token : string (default: None)
-    """
-    if (access_token is None or access_token == "" or 
-            access_token[:7] != "Bearer " or len(access_token[7:]) < 30):
-        resp = jsonify({"error": "Bad authorization header"})
-        resp.status_code = 400
-        return resp
-    else:
-        None
-
-
-def basic_system_id_checks(system_id=None):
-    """ Test validity of system ID
-
-    Check whether the system ID meets basic format requirements.
-
-    INPUT
-    -----
-    system_id : string (default: None)
-    """
-    if system_id is None or system_id == "":
-        resp = jsonify({"error": "No system ID specified"})
-        resp.status_code = 400
-        return resp
-    else:
-        return None
-
-
-def basic_file_path_checks(file_path=None):
-    """ Test validity of file path
-    
-    Check whether the system ID meets basic format requirements.
-    
-    INPUT
-    -----
-    system_id : string (default: None)
-    """
-    if file_path is None or file_path == "":
-        resp = jsonify({"error": "No path to file/dir specified"})
-        resp.status_code = 400
-        return resp
-    else:
-        return None
-
 
 
 class AgaveFilesMedia(Resource):
